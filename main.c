8bf9ca310c2dee84b2dc260c9a2700fbd030aada
fjp <jinpeng.f@outlook.com>
2021-10-15 20:44:26 +0800

    程序清单基本完成，第一次提交。

---
+++ main.c
@@ -0,0 +1,18 @@
+/* Copies a file */
+#include <stdio.h>
+#include <stdlib.h
+
+int main(int argc, char *argv[])
+{
+    FILE *source_fp, *dest_fp;
+    int ch;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: fcopy source dest\n");
+        exit (EXIT_FAILURE);
+    }
+
+    if (argc != 3) {
+
+    }
+}
